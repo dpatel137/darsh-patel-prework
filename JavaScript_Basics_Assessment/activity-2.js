33bc86edc36d8a0f9a999780814d1d1db15d7e05
Darsh <dpatel137@hawk.iit.edu>
2020-04-10 18:22:17 -0500

    activity 2

---
+++ JavaScript_Basics_Assessment/activity-2.js
@@ -0,0 +1,7 @@
+const user = prompt('What is your name?');
+if (user.length > 4) {
+    alert('Your name is greater than four chatacters');
+}
+else {
+    alert('Your name is less than four characters');
+}
